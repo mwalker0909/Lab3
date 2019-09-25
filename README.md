2022d744166a30331952718953b3ca818bed188e
mason <mwalker0909>
2019-09-25 12:00:09 -0700

    improving positioning of my ol and ul html

---
+++ README.md
@@ -0,0 +1,3 @@
+I have started by creating a no repo and copying much of my html / css / js into the new lab 3 section
+I then have reviewed and ensured that everything is connected and functioning properly.
+Next, I git added and git commit -m'd my work to ensure i'm set up properly here as well
--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 'use strict';
+var score = 0
 
 var userName = prompt('Please tell us who you are!');
 
@@ -10,6 +11,7 @@ if (one.toLowerCase() === 'yes') {
         // console.log('great! Please let me know how I did!'); 
         // I have used the comment on this log but I did alter the other console.logs into alerts. 
         alert( 'great! Please let me know how I did!');
+        score++;
 }else  { 
         alert( 'Well, ' + userName + ' you are pretty great for being in this class. We should work together to make more of this cool stuff!');
 }
@@ -17,6 +19,7 @@ if (one.toLowerCase() === 'yes') {
 var two = prompt('did you do the reading yesterday?');
 if (two.toLowerCase() === 'yes') {
         alert('Great work!');
+        score++;
 }   else  {
         alert('hmm, you probaby should complete that,' + userName);
 }
@@ -26,6 +29,7 @@ if (two.toLowerCase() === 'yes') {
 var three = prompt('did it take you a long time to get to class today? ');
 if (three.toLowerCase() === 'yes') {
         alert('Hmm, well we all cannot live as close as Calvin!');
+        score++;
 }   else   {
         alert('I bet you slept in then, huh ' + userName);
 }
@@ -35,6 +39,7 @@ if (three.toLowerCase() === 'yes') {
 var four = prompt ('Did you bring lunch today? ');
 if (four.toLowerCase() === 'yes') {
         alert('Yeah, I think I should bring lunch more often like you!');
+        score++;
 }   else {
         alert('I bet you could save a lot of money if you did,  ' + userName);
 }
@@ -42,7 +47,10 @@ if (four.toLowerCase() === 'yes') {
 var five = prompt ('Are you getting tired of these questions? ');
 if (five.toLowerCase() === 'yes') {
         alert('You and me both, ' + userName);
+        score++;
 }    else  {
         alert('oh really? I can make some more for you if you think you can handle it,  ' + userName); 
 
+//} do I need this here or should i remove the } ???
+alert('Hey congradulations! You answered ' + score + ' correctly!');
 }
