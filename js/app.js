0f3ef54d5b04f2d2029fb9dcfdce35c551d72a47
mason <mwalker0909>
2019-09-30 19:26:52 -0700

    good to go

--- js/app.js
+++ js/app.js
@@ -1,12 +1,9 @@
 'use strict';
 
- //<div class="box1" style = "margin-bottom: 0px"
-
-
+ 
 
 var score = 0;
 
-<<<<<<< HEAD
 var userName = prompt('Please tell us who you are!');
 
 alert( 'hello ' + userName + '! Welcome to my About Me html. Hope you enjoy it!');
@@ -27,70 +24,19 @@ if (two.toLowerCase() === 'yes') {
         score++;
 }   else  {
         alert('hmm, you probaby should complete that,' + userName);
-=======
-// var guessAttempts = 0;
-var score = 0;
-var userName = prompt('Please tell us who you are!');
-function gameOne(){
-  alert( 'hello ' + userName + '! Welcome to my About Me html. Hope you enjoy it!');
-
-  var one = prompt('Are you grading this today?');
-
-  if (one.toLowerCase() === 'yes') {
-    // console.log('great! Please let me know how I did!'); 
-    // I have used the comment on this log but I did alter the other console.logs into alerts. 
-    alert( 'great! Please let me know how I did!');
-    score++;
-  }else  { 
-    alert( 'Well, ' + userName + ' you are pretty great for being in this class. We should work together to make more of this cool stuff!');
-  }
-
-  var two = prompt('did you do the reading yesterday?');
-  if (two.toLowerCase() === 'yes') {
-    alert('Great work!');
-    score++;
-  }   else  {
-    alert('hmm, you probaby should complete that,' + userName);
-  }
-
-
-
-  var three = prompt('did it take you a long time to get to class today? ');
-  if (three.toLowerCase() === 'yes') {
-    alert('Hmm, well we all cannot live as close as Calvin!');
-    score++;
-  }   else   {
-    alert('I bet you slept in then, huh ' + userName);
-  }
-
-
-
-  var four = prompt ('Did you bring lunch today? ');
-  if (four.toLowerCase() === 'yes') {
-    alert('Yeah, I think I should bring lunch more often like you!');
-    score++;
-  }   else {
-    alert('I bet you could save a lot of money if you did,  ' + userName);
-  }
-        
-  var five = prompt ('Are you getting tired of these questions? ');
-  if (five.toLowerCase() === 'yes') {
-    alert('You and me both, ' + userName);
-    score++;
-  }    else  {
-    alert('oh really? I can make some more for you if you think you can handle it,  ' + userName); 
-    
-  } 
-  alert('Sooo I want to play a game');
->>>>>>> 43a616b8a1c503259d5fe772b5d6b84734fd6b4f
 }
-gameOne();
 
 
 
+var three = prompt('did it take you a long time to get to class today? ');
+if (three.toLowerCase() === 'yes') {
+        alert('Hmm, well we all cannot live as close as Calvin!');
+        score++;
+}   else   {
+        alert('I bet you slept in then, huh ' + userName);
+}
 
 
-<<<<<<< HEAD
 
 var four = prompt ('Did you bring lunch today? ');
 if (four.toLowerCase() === 'yes') {
@@ -133,38 +79,11 @@ if (five.toLowerCase() === 'yes') {
         alert('Sorry, no more tries!');
         alert('The correct answer was 5 by the way!');
  }
-=======
-var answer = 5;
-var isCorrect =false;
-var responseSix =parseInt(prompt('guess what number between one and ten i\'m thinking of. You have 4 tries!'));
-//  console.log(responseSix);
-function gameTwo(){
-  for (var i=1; i<5; i++){
-    if (responseSix === answer){
-      alert('Wow great guess! That is it!');
-      isCorrect = true;
-      score++;
-      alert('Hey you got '+ score + ' correct answers. great job!');
-      break;
-    }
-    if(responseSix < answer) alert('Too low! Try something higher!');
-    if(responseSix > answer) alert('Too high! Try something lower!');
-    responseSix= parseInt(prompt('That\'s not it! Guess again'));
-  }
-
-  if(!isCorrect) {
-    alert('Sorry, no more tries!');
-    alert('The correct answer was 5 by the way!');
-  }
-}
-gameTwo();
->>>>>>> 43a616b8a1c503259d5fe772b5d6b84734fd6b4f
 
 var colors = ['red', 'white', 'blue'];
 var count = 0
 
 
-<<<<<<< HEAD
 
 while(count < 6 ) {
         var question = prompt('I\'m thinking of three colors. I will give you six guesses to pick one of them! give it your best shot!');
@@ -183,38 +102,10 @@ while(count < 6 ) {
         if(comment === false){
                 alert('sorry, try again');
                 count++;
-        if (count === 6) {
+        if(count === 6) {
                 alert('sorry, the right answers were red, white, or blue!');
         }
         }
 }
-alert('Hey you got '+ score + ' correct answers. great job!');
-=======
-function gameThree(){
-  while(count < 6 ) {
-    var question = prompt('Hey, I\'m thinking of three colors. I will give you six guesses to find one!');
-    var comment = false;        
-    for(var i=0; i<6; i++) {
-      if(question === colors[i]) {
-        comment = true;
-      }
 
-    }
-    if( comment === true){
-      alert('congrats');
-      score++
-      alert('Hey you got '+ score + ' correct answers. great job!');
-      break;
-    }
-    if(comment === false){
-      alert('sorry, try again');
-      count++;
-      if (count === 6) {
-        alert('sorry, the right answers were red, white, or blue!');
-        alert('Hey you got '+ score + ' correct answers. great job!');
-      }
-    }
-  }
-}
-gameThree();
->>>>>>> 43a616b8a1c503259d5fe772b5d6b84734fd6b4f
+alert('Hey you got '+ score + ' correct answers. great job!');
